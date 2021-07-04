2365ceafec4a3798cfaa095eddb4bd2e1505d135
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-07-04 15:24:47 -0500

    footer compenent implemented

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+
+class Foot extends Component {
+    render () {
+        return (
+            <Card className="text-center">
+                        <Card.Title>Made in Round Rock, Texas</Card.Title>
+            </Card>
+        );
+    }
+}
+export default Foot;
