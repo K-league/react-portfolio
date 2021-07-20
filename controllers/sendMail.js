285cc1163f7357dd15e8f7914087dc663b4c9c1a
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-07-20 11:49:16 -0500

    reconfigured createTransport obj

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 
-nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
     host: "gmail.com",
     //BE 8000, FE 3000
     port: 8000,
